fb7987fa95348abc76179f8a6e32f171a21eac22
kojiro629 <kojiro629@gmail.com>
2024-03-16 01:42:01 +0900

    update alma9_httpd.ps1

--- alma9_httpd/alma9_httpd.ps1
+++ alma9_httpd/alma9_httpd.ps1
@@ -1,5 +1,9 @@
+#-------------------------------------------------------------------------------
+# AlmaLinux9をベースにしてApacheをインストールしたイメージを作り、ホスト側の
+# サンプルHTMLの入ったフォルダをDocumentRoot(/var/www/html)にマウントする。
+#-------------------------------------------------------------------------------
 # イメージの作成
 docker build . -t alma9_httpd
 
 # コンテナの作成と実行
-docker run --name alma9_test -p 10088:80 -it --mount type=bind,source={DocumentRoot},target=/var/www/html alma9_httpd
+docker run --name alma9_test -p 10088:80 -it --mount type=bind,source={HTML folder},target=/var/www/html alma9_httpd
